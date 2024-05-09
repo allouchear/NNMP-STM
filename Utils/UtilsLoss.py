0dcba40ce4561b9b54a013203afe2b4dffb7a242
allouchear <allouchear@gmail.com>
2024-05-09 11:27:30 +0200

    Update Utils/UtilsLoss.py, SSIM_MAE, alpha, minmae, power_factors as options

--- Utils/UtilsLoss.py
+++ Utils/UtilsLoss.py
@@ -26,7 +26,7 @@ def stm_ssim_loss(model_stm ,target_stm, nx, ny):
 	loss = 1-ssim
 	return loss
 
-def stm_ms_ssim (model_stm ,target_stm, nx, ny):
+def stm_ms_ssim (model_stm ,target_stm, nx, ny,power_factors=(0.0448, 0.2856, 0.3001, 0.2363, 0.1333)):
 	model_stm_image  = tf.reshape(model_stm,[model_stm.shape[0],nx,ny,1])
 	target_stm_image = tf.reshape(target_stm, [target_stm.shape[0],nx,ny,1])
 	max_val = 1.0
@@ -37,7 +37,7 @@ def stm_ms_ssim (model_stm ,target_stm, nx, ny):
 		model_stm_image /= maxm-minm
 	# Return A tensor containing an MS-SSIM value for each image in batch. The values are in range [0, 1]. 
       # Returns a tensor with shape: broadcast(img1.shape[:-3], img2.shape[:-3]). 
-	ssim_ms = tf.image.ssim_multiscale(model_stm_image, target_stm_image, max_val=max_val, power_factors=(0.0448, 0.2856,0.1333),
+	ssim_ms = tf.image.ssim_multiscale(model_stm_image, target_stm_image, max_val=max_val, power_factors=power_factors,
 	filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
 
 	return ssim_ms
@@ -60,7 +60,7 @@ def stm_rmse_loss(model_stm ,target_stm):
 	loss = (model_stm-target_stm)*(model_stm-target_stm)
 	return loss
 
-def stm_loss(model_stm ,target_stm, nx, ny, threshold=THRFREQ, loss_bypixel=0, loss_type='SID',alpha=0.84):
+def stm_loss(model_stm ,target_stm, nx, ny, threshold=THRFREQ, loss_bypixel=0, loss_type='SID'):
 	if len( model_stm.shape)==1:
 		 model_stm = tf.reshape(model_stm,[1,-1])
 		 target_stm = tf.reshape(target_stm,[1,-1])
@@ -101,14 +101,31 @@ def stm_loss(model_stm ,target_stm, nx, ny, threshold=THRFREQ, loss_bypixel=0, l
 
 	elif loss_type.upper()=='MS_SSIM':
 		loss=stm_loss_ms_ssim_all(model_stm ,target_stm, nx, ny)
-	elif loss_type.upper()=='MS_SSIM_MAE':
-		loss = stm_loss_ms_ssim_mean(model_stm ,target_stm, nx, ny,alpha=alpha)
+	elif 'MS_SSIM_MAE' in loss_type.upper():
+		alpha=0.84
+		stl=loss_type.upper().split()
+		if len(stl)>1:
+			alpha=float(stl[1])
+		minmae=1000.0
+		if len(stl)>2:
+			minmae=float(stl[2])
+		power_factors=(0.0448, 0.2856, 0.3001, 0.2363, 0.1333)
+		if len(stl)>3:
+			pf=stl[3].split(',')
+			power_factors=tuple(map(float,pf))	
+		loss = stm_loss_ms_ssim_mean(model_stm ,target_stm, nx, ny,alpha=alpha, minmae=minmae, power_factors=power_factors)
+	elif 'SSIM_MAE' in loss_type.upper():
+		alpha=0.84
+		stl=loss_type.upper().split()
+		if len(stl)>1:
+			alpha=float(stl[1])
+		loss = stm_loss_ssim_mean(model_stm ,target_stm, nx, ny,alpha=alpha)
 	else:
-		print("************************************************************")
+		print("***********************************************************************")
 		print("ERROR : Unknown loss type ",loss_type)
 		print("        Calculation stopped")
-		print("        Use SID, MAE , RMSE , SSIM , MS_SSIM or MS_SSIM_MAE")
-		print("************************************************************")
+		print("        Use SID, MAE , RMSE , SSIM , MS_SSIM , SSIM_MAE, or MS_SSIM_MAE")
+		print("***********************************************************************")
 		sys.exit()
 
 	loss=tf.reduce_mean(loss) # loss = mean of loss structures. loss of a struture = sum of difference on all pixels if loss=tf.reduce_sum(loss,axis=1) , by pixels if tf.reduce_mean
@@ -195,7 +212,7 @@ def stm_ms_ssim_all(model_stm ,target_stm, nx, ny):
 	ms_ssim=tf.Variable(ms_ssim,dtype=model_stm.dtype)
 	return ms_ssim
 
-def stm_loss_ms_ssim_all(model_stm ,target_stm, nx, ny):
+def stm_loss_ms_ssim_all(model_stm ,target_stm, nx, ny,power_factors=(0.0448, 0.2856, 0.3001, 0.2363, 0.1333)):
 	if len( model_stm.shape)==1:
 		 model_stm = tf.reshape(model_stm,[1,-1])
 		 target_stm = tf.reshape(target_stm,[1,-1])
@@ -210,7 +227,7 @@ def stm_loss_ms_ssim_all(model_stm ,target_stm, nx, ny):
 		model_stmi = model_stmi[0:m]
 		target_stmi = tf.reshape(target_stmi,[1,-1])
 		model_stmi = tf.reshape(model_stmi,[1,-1])
-		ms_ssimi=stm_ms_ssim(model_stmi ,target_stmi, nx[i], ny[i])
+		ms_ssimi=stm_ms_ssim(model_stmi ,target_stmi, nx[i], ny[i],power_factors=power_factors)
 		ms_ssimi=tf.reduce_mean(ms_ssimi)
 		if ms_ssim is None:
 			ms_ssim = ms_ssimi
@@ -227,13 +244,26 @@ def stm_loss_get_mean(model_stm ,target_stm):
 	loss_mae = tf.reduce_mean(loss_mae)
 	return loss_mae
 
-def stm_loss_ms_ssim_mean(model_stm ,target_stm, nx, ny,alpha=0.84):
+# https://arxiv.org/pdf/1511.08861, default alpha=0.84
+def stm_loss_ms_ssim_mean(model_stm ,target_stm, nx, ny,alpha=0.84,minmae=1000.0, power_factors=(0.0448, 0.2856, 0.3001, 0.2363, 0.1333)):
 	loss_mae = stm_loss_get_mean(model_stm ,target_stm)
-	if  loss_mae<1000.0:
-		loss_ms_ssim = stm_loss_ms_ssim_all(model_stm ,target_stm, nx, ny)
+	if  loss_mae<minmae:
+		print("power_factors     = ",power_factors)
+		loss_ms_ssim = stm_loss_ms_ssim_all(model_stm ,target_stm, nx, ny,power_factors=power_factors)
 	else:
 		loss_ms_ssim = stm_loss_ssim_all(model_stm ,target_stm, nx, ny)
-	print("loss ssim = ",loss_ms_ssim.numpy())
-	print("loss mae  = ",loss_mae.numpy())
+	print("alpha             = ",alpha)
+	print("min mae to use ms = ",minmae)
+	print("loss ms ssim      = ",loss_ms_ssim.numpy())
+	print("loss mae          = ",loss_mae.numpy())
 	loss =  alpha*loss_ms_ssim+(1.0-alpha)*loss_mae
+	return loss
+
+def stm_loss_ssim_mean(model_stm ,target_stm, nx, ny,alpha=0.84):
+	loss_mae = stm_loss_get_mean(model_stm ,target_stm)
+	loss_ssim = stm_loss_ssim_all(model_stm ,target_stm, nx, ny)
+	print("alpha     = ",alpha)
+	print("loss_ssim = ",loss_ssim.numpy())
+	print("loss_mae  = ",loss_mae.numpy())
+	loss =  alpha*loss_ssim+(1.0-alpha)*loss_mae
 	return loss
